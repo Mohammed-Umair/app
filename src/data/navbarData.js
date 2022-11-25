ce546103ff76e0d8d280b3ed0ac962e96aa33d5b
Mohammed Umair <94831461+Mohammed-Umair@users.noreply.github.com>
2022-11-25 17:53:47 +0530

    finalupdate

--- src/data/navbarData.js
+++ src/data/navbarData.js
@@ -1,5 +1,5 @@
 export const navbarMenu = [
-    { id: 1, path: "/", title: "Novel" },
+    { id: 1, path: "/app", title: "Novel" },
     { id: 2, path: "#narrative", title: "Narrative" },
     { id: 3, path: "#science", title: "Science Fiction" },
     { id: 4, path: "#history", title: "History" },
